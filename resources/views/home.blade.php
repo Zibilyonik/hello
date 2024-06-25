c4413013bb5b8e265b668a6f17f655321ededdae
Zibilyonik <onatsurmeli@gmail.com>
2024-06-25 09:54:46 +0200

    chore: Add job listings to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,7 @@
         Home
     </x-slot:heading>
     <h1>Helloing Home</h1>
+    @foreach ($jobs as $job)
+        <li><strong>{{ $job['title'] }}</strong>, pays {{ $job['salary'] }}</li>
+    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'jobs' => [
+            [
+                'title' => 'Bobber',
+                'salary' => '10 Monies'
+            ],
+            [
+                'title' => 'Johner',
+                'salary' => '4 Monies'
+            ],
+            [
+                'title' => 'Peacher',
+                'salary' => '7 Monies'
+            ],
+            [
+                'title' => 'Vincentman',
+                'salary' => '8 Monies'
+            ],
+            [
+                'title' => 'Pauler',
+                'salary' => '2 Monies'
+            ]
+        ]
+    ]);
 });
 
 Route::get('/about', function () {
