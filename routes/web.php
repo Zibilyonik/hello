882643a590cf40c64ac99db73532f3485d21fe4d
Onur <onatsurmeli@gmail.com>
2024-07-05 21:22:08 +0200

    Move jobs list into Job class, adjust get route jobs access

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,11 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
+class Job
+{
+    public static function all():array
+    {
+        return [
             [
                 'id' => 1,
                 'title' => 'Bobber',
@@ -39,39 +37,23 @@ Route::get('/jobs', function () {
                 'title' => 'Pauler',
                 'salary' => '2 Monies'
             ]
-        ]
+        ];
+    }
+}
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Bobber',
-            'salary' => '10 Monies'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Johner',
-            'salary' => '4 Monies'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Peacher',
-            'salary' => '7 Monies'
-        ],
-        [
-            'id' => 4,
-            'title' => 'Vincentman',
-            'salary' => '8 Monies'
-        ],
-        [
-            'id' => 5,
-            'title' => 'Pauler',
-            'salary' => '2 Monies'
-        ]
-    ];
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    $job = Arr::first(Job::all(), fn ($job) => $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
 
